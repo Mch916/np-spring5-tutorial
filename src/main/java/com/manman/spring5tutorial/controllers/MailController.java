b6570a1cbc6e0f799dd7cb1565e1287e290bf6b2
Mch916 <chunho1992@gmail.com>
2018-08-23 15:58:14 +0800

    Initial commit

---
+++ src/main/java/com/manman/spring5tutorial/controllers/MailController.java
@@ -0,0 +1,23 @@
+package com.manman.spring5tutorial.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.manman.spring5tutorial.mail.MailSender;
+import com.manman.spring5tutorial.mail.MockMailSender;
+
+@RestController
+public class MailController {
+
+	@Autowired
+	private MailSender mailSender;
+	
+	@RequestMapping("/mail")
+	public String mail() {
+		
+		mailSender.send("mail@example.com", "A test mail", "Body of the mail");
+		
+		return "Mail Sent";
+	}
+}
---
+++ src/main/java/com/manman/spring5tutorial/mail/MailSender.java
@@ -0,0 +1,5 @@
+package com.manman.spring5tutorial.mail;
+
+public interface MailSender {
+	void send(String to, String subject, String body);
+}
